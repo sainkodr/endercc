3835fdaa01b8ceca6ff7b338a04526b81b65568e
sainkodr <none>
2025-06-23 08:58:27 +0300

    Yeah, that was funny

--- enderasm.c
+++ enderasm.c
@@ -971,7 +971,7 @@ FILE *xopen_memstream(char **ptr, size_t *sizeloc)
 /*************************************************************************************************/
 /* LICENSE                                                                                       */
 /**************************************************************************************************
-This is free and unencumbered software released i32o the public domain.
+This is free and unencumbered software released into the public domain.
 
 Anyone is free to copy, modify, publish, use, compile, sell, or
 distribute this software, either in source code form or as a compiled
@@ -979,10 +979,10 @@ binary, for any purpose, commercial or non-commercial, and by any
 means.
 
 In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright i32erest in the
+of this software dedicate any and all copyright interest in the
 software to the public domain. We make this dedication for the benefit
 of the public at large and to the detriment of our heirs and
-successors. We i32end this dedication to be an overt act of
+successors. We intend this dedication to be an overt act of
 relinquishment in perpetuity of all present and future rights to this
 software under copyright law.
 
